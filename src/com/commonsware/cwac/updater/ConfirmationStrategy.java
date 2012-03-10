41da27b099ae02a30c1e63e8bca7f07906119cf1
Mark Murphy <mmurphy@xps15.(none)>
2012-03-10 14:26:56 -0500

    added JavaDocs

--- src/com/commonsware/cwac/updater/ConfirmationStrategy.java
+++ src/com/commonsware/cwac/updater/ConfirmationStrategy.java
@@ -18,6 +18,26 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.os.Parcelable;
 
+/**
+ * Interface for confirming a step (download an APK,
+ * install an APK). 
+ *
+ */
 public interface ConfirmationStrategy extends Parcelable {
+  /**
+   * Called by UpdateService to confirm whether or not
+   * it is OK to proceed to the next phase in the
+   * update process. Will be called on a background thread
+   * and so cannot directly affect the UI. If you know
+   * that it is OK to proceed (e.g., always or based on
+   * a preference), return true. If you need to get 
+   * input from the user (e.g., Notification, dialog-themed
+   * Activity), return false, but then execute the supplied
+   * PendingIntent if the user agrees to go ahead.
+   * 
+   * @param ctxt generic Context object for raising Notification, etc.
+   * @param contentIntent PendingIntent to be used if confirmation is asynchronous
+   * @return true if we can go ahead, false if we don't know yet
+   */
   public boolean confirm(Context ctxt, PendingIntent contentIntent);
 }
