5ae8170398be6d1f4e07b5e7d13d8b26d9e792e7
Mark Murphy <mmurphy@xps15.(none)>
2012-03-12 15:52:22 -0400

    added JavaDocs

--- src/com/commonsware/cwac/updater/NotificationConfirmationStrategy.java
+++ src/com/commonsware/cwac/updater/NotificationConfirmationStrategy.java
@@ -21,19 +21,35 @@ import android.content.Context;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+/**
+ * Implementation of ConfirmationStrategy that raises
+ * a Notification to alert user that we are ready to
+ * move to the next step.
+ */
 public class NotificationConfirmationStrategy implements
     ConfirmationStrategy {
   private static final int NOTIFICATION_ID=99369921;
   private Notification notification=null;
 
+  /**
+   * Public constructor
+   * @param notification Notification to raise (complete with icon, etc.)
+   */
   public NotificationConfirmationStrategy(Notification notification) {
     this.notification=notification;
   }
   
+  /**
+   * Private constructor for use in Parcelable implementation
+   * @param in Parcel to restore instance from
+   */
   private NotificationConfirmationStrategy(Parcel in) {
     notification=in.readParcelable(null);
   }
 
+  /* (non-Javadoc)
+   * @see com.commonsware.cwac.updater.ConfirmationStrategy#confirm(android.content.Context, android.app.PendingIntent)
+   */
   @Override
   public boolean confirm(Context ctxt, PendingIntent contentIntent) {
     NotificationManager mgr=
@@ -46,16 +62,27 @@ public class NotificationConfirmationStrategy implements
     return(false);
   }
 
+  /* (non-Javadoc)
+   * @see android.os.Parcelable#describeContents()
+   */
   @Override
   public int describeContents() {
     return(0);
   }
 
+  /* (non-Javadoc)
+   * @see android.os.Parcelable#writeToParcel(android.os.Parcel, int)
+   */
   @Override
   public void writeToParcel(Parcel dest, int flags) {
     dest.writeParcelable(notification, 0);
   }
 
+  /**
+   * Required to complete Parcelable interface. Creates
+   * an NotificationConfirmationStrategy instance or array
+   * upon demand.
+   */
   public static final Parcelable.Creator<NotificationConfirmationStrategy> CREATOR=
       new Parcelable.Creator<NotificationConfirmationStrategy>() {
         public NotificationConfirmationStrategy createFromParcel(Parcel in) {
