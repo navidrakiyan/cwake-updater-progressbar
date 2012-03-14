5df00b73065726c7f208f9defd0e4da5cc806d2d
Mark Murphy <mmurphy@xps15.(none)>
2012-03-14 09:15:17 -0400

    added JavaDocs

--- src/com/commonsware/cwac/updater/VersionCheckStrategy.java
+++ src/com/commonsware/cwac/updater/VersionCheckStrategy.java
@@ -16,7 +16,36 @@ package com.commonsware.cwac.updater;
 
 import android.os.Parcelable;
 
+/**
+ * Interface for checking to see if an update is available
+ * for this particular application.
+ * 
+ */
 public interface VersionCheckStrategy extends Parcelable {
+  /**
+   * Retrieves the version code of any available update.
+   * This method will be called first on this object, so it
+   * can (and should) also cache any other information to be
+   * returned by other getters (e.g., the update URL for
+   * getUpdateURL()).
+   * 
+   * @return the version code of the application, or -1 if
+   *         there is no update
+   * @throws Exception
+   *           in case something goes wrong
+   */
   int getVersionCode() throws Exception;
+
+  /**
+   * Returns a String to be (possibly) used by the
+   * DownloadStrategy to retrieve this update. In theory,
+   * this is a URL (hence the method name); in practice, it
+   * can be whatever is necessary, or even the empty string
+   * if nothing is required.
+   * 
+   * @return a URL or other download information
+   * @throws Exception
+   *           in case something goes wrong
+   */
   String getUpdateURL() throws Exception;
 }
