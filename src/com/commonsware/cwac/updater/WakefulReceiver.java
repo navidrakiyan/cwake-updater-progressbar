db0cbf5548d418040f5f48fe86043753da181fbd
Mark Murphy <mmurphy@xps15.(none)>
2012-03-15 10:34:22 -0400

    added JavaDocs

--- src/com/commonsware/cwac/updater/WakefulReceiver.java
+++ src/com/commonsware/cwac/updater/WakefulReceiver.java
@@ -19,8 +19,27 @@ import android.content.Context;
 import android.content.Intent;
 import com.commonsware.cwac.wakeful.WakefulIntentService;
 
+/**
+ * Forwards a command (an Intent packaged as an extra in the
+ * broadcast Intent) along to a WakefulIntentService,
+ * specifically UpdateService.
+ * 
+ * This is necessary to support asynchronous user
+ * confirmation, such as via a Notification. We need the
+ * UpdateService to be able to continue to the next phase,
+ * but the UpdateService needs to be a WakefulIntentService
+ * to have a shot at completing its work. Hence, we cannot
+ * simply have the Notification use a getService()
+ * PendingIntent pointing at UpdateService -- instead, we
+ * need to go through the standard WakefulIntentService
+ * process, involving a BroadcastReceiver calling
+ * sendWakefulWork().
+ * 
+ */
 public class WakefulReceiver extends BroadcastReceiver {
-
+  /* (non-Javadoc)
+   * @see android.content.BroadcastReceiver#onReceive(android.content.Context, android.content.Intent)
+   */
   @Override
   public void onReceive(Context context, Intent intent) {
     Intent cmd=
